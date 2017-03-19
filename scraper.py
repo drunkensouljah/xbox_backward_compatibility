721c39af688d6892edcee28a20f081ccc0fc07b8
Daniel Blatt <drunkensouljah@users.noreply.github.com>
2017-03-19 21:29:26 +0100

    init commit ✌️

--- scraper.py
+++ scraper.py
@@ -1,24 +1,20 @@
 # This is a template for a Python scraper on morph.io (https://morph.io)
-# including some code snippets below that you should find helpful
+import scraperwiki
+import lxml.html
 
-# import scraperwiki
-# import lxml.html
-#
-# # Read in a page
-# html = scraperwiki.scrape("http://foo.com")
-#
-# # Find something on the page using css selectors
-# root = lxml.html.fromstring(html)
-# root.cssselect("div[align='left']")
-#
-# # Write out to the sqlite database using scraperwiki library
-# scraperwiki.sqlite.save(unique_keys=['name'], data={"name": "susan", "occupation": "software developer"})
-#
-# # An arbitrary query against the database
-# scraperwiki.sql.select("* from data where 'name'='peter'")
+html = scraperwiki.scrape("https://majornelson.com/blog/xbox-one-backward-compatibility/")
 
-# You don't have to do things with the ScraperWiki and lxml libraries.
-# You can use whatever libraries you want: https://morph.io/documentation/python
-# All that matters is that your final data is written to an SQLite database
-# called "data.sqlite" in the current working directory which has at least a table
-# called "data".
+root = lxml.html.fromstring(html)
+i = 1
+for tr in root.cssselect("tbody tr"):
+    tds = tr.cssselect("td")
+    data = {
+        'id': i,
+        'game': tds[0].text_content(),
+        'typ': tds[1].text_content(),
+        'publisher': tds[2].text_content(),
+        'status': tds[3].text_content(),
+    }
+    scraperwiki.sqlite.save(unique_keys=['id'], data=data)
+    i += 1
+    print data
